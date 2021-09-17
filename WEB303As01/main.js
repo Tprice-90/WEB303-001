337cbba1eaeaed4d207c04ea70e3d99d8b1f4c83
Tprice-90 <90633341+Tprice-90@users.noreply.github.com>
2021-09-17 10:43:17 -0400

    Uploaded completed Assignment 1 file

--- WEB303As01/main.js
+++ WEB303As01/main.js
@@ -6,3 +6,17 @@
 
 "use strict";
 
+$(document).ready(function(){
+	var $salary = $('#salary');
+	let $percent = $('#percent');
+	let $spend = $('#spend').text();
+	
+	$(salary).on('change', function(event) {
+		$(spend).text('$' + $(salary).val());
+	});
+	
+	$(percent).on('change', function(event) {
+		$(spend).text('$' + $(salary).val() * $(percent).val() /100);
+		// $(spend).text(parseInt($(salary).val()) * parseInt($(percent).val()) / 100);
+	});
+});
