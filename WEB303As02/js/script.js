b541983828ca7aad78e3421fa0c7b942c5eadedc
Tprice-90 <90633341+Tprice-90@users.noreply.github.com>
2021-09-27 14:46:49 -0400

    Continual debudding of slideDown...

--- WEB303As02/js/script.js
+++ WEB303As02/js/script.js
@@ -2,25 +2,25 @@
 // Terry Price #0622422
 
 $(function (){
-	$('div#solution').css('height', '40px');
+	$('div#solution').css('height', '40px').hide();
 
 	$('a#vprospect').on('click', function(){
 
-		$('div#solution').hide().slideDown(4000, function(){
+		$('div#solution').slideDown(4000, function(){
 			$(this).load('prospect.html');
 		});
 	});
 
 	$('a#vconvert').on('click', function(){
 		
-		$('div#solution').hide().slideDown(4000, function(){
+		$('div#solution').slideDown(4000, function(){
 			$(this).load('convert.html');
 		});
 	});
 
 	$('a#vretain').on('click', function(){
 
-		$('div#solution').hide().slideDown(4000, function(){
+		$('div#solution').slideDown(4000, function(){
 			$(this).load('retain.html');
 		});
 	});
