c1760d3ad86d0f5d8ea76f27e0fc8fb74df75e08
Tprice-90 <90633341+Tprice-90@users.noreply.github.com>
2021-09-27 15:02:46 -0400

    never giving up on this.

--- WEB303As02/js/script.js
+++ WEB303As02/js/script.js
@@ -2,20 +2,20 @@
 // Terry Price #0622422
 
 $(function (){
-	$('div#solution').css({'height' : '40px' , 'display' : 'none'});
+	$('div#solution').css({'height' : '400px' , 'display' : 'none'});
 
 	$('a#vprospect').on('click', function(){
 
-		$('div#solution').slideDown(3000).load('prospect.html');
+		$('div#solution').hide().slideDown(3000).load('prospect.html');
 	});
 
 	$('a#vconvert').on('click', function(){
 		
-		$('div#solution').slideDown(3000).load('convert.html');
+		$('div#solution').hide().slideDown(3000).load('convert.html');
 	});
 
 	$('a#vretain').on('click', function(){
 
-		$('div#solution').slideDown(3000).load('retain.html');
+		$('div#solution').hide().slideDown(3000).load('retain.html');
 	});
 });
