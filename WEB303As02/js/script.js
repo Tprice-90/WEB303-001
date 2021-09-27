370b417c304ad3b9ea6ebb62e53acbd27e1d510d
Tprice-90 <90633341+Tprice-90@users.noreply.github.com>
2021-09-27 14:21:15 -0400

    Added remaining link loads and slideDown

--- WEB303As02/js/script.js
+++ WEB303As02/js/script.js
@@ -2,8 +2,20 @@
 // Terry Price #0622422
 
 $(function (){
+	$('div#solution').css('height', '100px');
+
+	$('#a#vprospect').on('click', function(){
+
+		$('div#solution').load('prospect.html').slideDown(3000);
+	});
+
 	$('a#vconvert').on('click', function(){
 		
-		$('div#solution').load('convert.html');
+		$('div#solution').load('convert.html').slideDown(3000);
+	});
+
+	$('a#vretain').on('click', function(){
+
+		$('div#solution').load('retain.html').slideDown(3000);
 	});
 });
