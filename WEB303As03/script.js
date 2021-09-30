21c66dfce76e17d06c912eb4791a6d3b08c7d95e
Tprice-90 <90633341+Tprice-90@users.noreply.github.com>
2021-09-30 13:33:03 -0400

    Added Assignment 3 w/ getJSON

---
+++ WEB303As03/script.js
@@ -0,0 +1,13 @@
+// WEB303 Assignment 3
+//Terry Price #0622422
+
+$(function () {
+	$.getJSON("team.json").done(function (data) {
+		$.each(data.teammembers, function (index, value) {
+			$(`div#team${index}`).html(`
+			<h3>${value.name}</h3>
+			<h4>${value.title}</h4>
+			<p>${value.bio}</p>`);
+		});
+	});
+});
