d6b7cd54002346a5f86fff98c5f5c731685a8c14
Tprice-90 <90633341+Tprice-90@users.noreply.github.com>
2021-10-04 23:15:51 -0400

    completed script file uploaded

--- WEB303As03/script.js
+++ WEB303As03/script.js
@@ -2,13 +2,37 @@
 //Terry Price #0622422
 
 $(function () {
-	$.getJSON("team.json").done(function (data) {
+	var teammemberReturnValue = $.getJSON("team.json").done(function (data) {
 		$.each(data.teammembers, function(index, value) {
-			$(`#team${index}`).html(`
+			$(`div#team`).append(`
 			<h3>${value.name}</h3>
 			<h4>${value.title}</h4>
 			<p>${value.bio}</p>
 			`);
 		});
+		console.log("Teammember return value: ", teammemberReturnValue);
+	});
+	
+	$.ajax({
+		url:"team.json",
+		beforeSend: function(){
+			$("div#team").html(`
+			<h3>Loading...</h3>
+			`);
+		},
+		error: function(xhr, error) {
+			alert(xhr, error.message);
+		},
+		timeout: 5000,
+		success: function(data) {
+			$.each(data.teammembers, function(index, value) {
+				$(`div#team`).append(`
+				<h3>${value.name}</h3>
+				<h4>${value.team}</h4>
+				<p>${value.bio}</p>
+				`);
+			});
+			$(`div#team h3`).remove();
+		},
 	});
 });
